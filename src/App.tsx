a9f5af38084d6880a3cdda2df1d74e78502db61d
Dar Levy <darlevy8@gmail.com>
2023-11-19 00:37:30 +0200

    Add Delete button to To Do item component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from "./header/Header.tsx";
+import ToDoItem from "./body/to-dos/to-do/ToDoItem.tsx";
 
 function App() {
   return (
     <>
       <Header/>
+    <ToDoItem/>
     </>
   )
 }
--- src/body/to-dos/to-do/ToDoItem.tsx
+++ src/body/to-dos/to-do/ToDoItem.tsx
@@ -1,7 +1,11 @@
+import DeleteButton from "./delete-button/DeleteButton.tsx";
 
 function ToDoItem() {
   return (
-      <li>Buy milk</li>
+      <>
+        <li>Buy milk</li>
+        <DeleteButton/>
+      </>
   )
 }
 
