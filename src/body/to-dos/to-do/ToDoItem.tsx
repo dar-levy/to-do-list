5bab389881390e85e73d2e628530399ea16d0c54
Dar Levy <darlevy8@gmail.com>
2023-11-19 01:09:31 +0200

    Remove redundant styling

--- src/body/to-dos/to-do/ToDoItem.tsx
+++ src/body/to-dos/to-do/ToDoItem.tsx
@@ -2,10 +2,10 @@ import DeleteButton from "./delete-button/DeleteButton.tsx";
 
 function ToDoItem() {
   return (
-      <div className="to-do-item">
-        <li>Buy milk</li>
+    <li className="to-do-item">
+        Buy milk
         <DeleteButton/>
-      </div>
+    </li>
   )
 }
 
