e78b413ac0bc55975f484daf269220d5cd54d41a
Dar Levy <darlevy8@gmail.com>
2023-11-19 00:51:42 +0200

    Create to do css

--- src/body/to-dos/to-do/ToDoItem.tsx
+++ src/body/to-dos/to-do/ToDoItem.tsx
@@ -2,7 +2,7 @@ import DeleteButton from "./delete-button/DeleteButton.tsx";
 
 function ToDoItem() {
   return (
-      <div>
+      <div className="to-do-item">
         <li>Buy milk</li>
         <DeleteButton/>
       </div>
