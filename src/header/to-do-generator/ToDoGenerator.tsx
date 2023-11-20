cad5dbd77c6a1c3f8c5ec6d2bd90291ad3e0d5b7
Dar Levy <darlevy8@gmail.com>
2023-11-20 12:38:14 +0200

    Syle to do generator component

--- src/header/to-do-generator/ToDoGenerator.tsx
+++ src/header/to-do-generator/ToDoGenerator.tsx
@@ -1,8 +1,10 @@
+import "./ToDoGenerator.css";
+
 const ToDoGenerator = () => {
   return (
     <div className="to-do-generator">
       <input type="text" placeholder="Add a new to do" />
-      <span className="addBtn" />
+      <span className="addBtn">Add</span>
     </div>
   );
 };
