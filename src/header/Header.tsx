b03dc63274834fdbeab2ff6d95b77f069b49a49a
Dar Levy <darlevy8@gmail.com>
2023-11-20 12:32:42 +0200

    Add input and a button for addition

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,9 +1,13 @@
-import "./Header.css"
+import "./Header.css";
+import ToDoGenerator from "./to-do-generator/ToDoGenerator.tsx";
 
 function Header() {
-    return <div className="header">
-        <h1>To-Do List</h1>
+  return (
+    <div id="myDiv" className="header">
+      <h1>To-Do List</h1>
+      <ToDoGenerator />
     </div>
+  );
 }
 
-export default Header
+export default Header;
--- src/header/to-do-generator/ToDoGenerator.tsx
+++ src/header/to-do-generator/ToDoGenerator.tsx
@@ -1,5 +1,10 @@
 const ToDoGenerator = () => {
-  return <div>ToDoGenerator</div>;
+  return (
+    <div className="to-do-generator">
+      <input type="text" placeholder="Add a new to do" />
+      <span className="addBtn" />
+    </div>
+  );
 };
 
 export default ToDoGenerator;
