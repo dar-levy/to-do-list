d66704e2f788d467c44087b04b107b07c7607556
Dar Levy <darlevy8@gmail.com>
2024-08-19 15:21:15 +0300

    Create html

--- src/app/home/to-dos/to-dos.component.ts
+++ src/app/home/to-dos/to-dos.component.ts
@@ -1,10 +1,16 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray} from "@angular/cdk/drag-drop";
+import {NgClass} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-to-dos',
   standalone: true,
-  imports: [],
+  imports: [
+    CdkDropList,
+    NgClass,
+    FormsModule
+  ],
   templateUrl: './to-dos.component.html',
   styleUrl: './to-dos.component.css'
 })
