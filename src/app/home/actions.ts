5419d8b3096928df20b76ac6011f8a822106c10e
Dar Levy <darlevy8@gmail.com>
2024-08-19 22:16:56 +0300

    Format actions

--- src/app/home/actions.ts
+++ src/app/home/actions.ts
@@ -1,21 +1,6 @@
 import { createAction, props } from '@ngrx/store';
 
-export const ADD_TODO = createAction(
-  '[ToDo] Add ToDo',
-  props<{ description: string }>()
-);
-
-export const TOGGLE_TODO = createAction(
-  '[ToDo] Toggle ToDo',
-  props<{ id: string }>()
-);
-
-export const REMOVE_TODO = createAction(
-  '[ToDo] Remove ToDo',
-  props<{ id: string }>()
-);
-
-export const EDIT_TODO = createAction(
-  '[ToDo] Edit ToDo',
-  props<{ id: string, description: string }>()
-);
+export const ADD_TODO = createAction('[ToDo] Add ToDo', props<{ description: string }>());
+export const TOGGLE_TODO = createAction('[ToDo] Toggle ToDo', props<{ id: string }>());
+export const REMOVE_TODO = createAction('[ToDo] Remove ToDo', props<{ id: string }>());
+export const EDIT_TODO = createAction('[ToDo] Edit ToDo', props<{ id: string, description: string }>());
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideStore } from '@ngrx/store';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
 import {AppComponent} from "./app/app.component";
-import {todosReducer} from "./app/home/store";
 import {appConfig} from "./app/app.config";
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
