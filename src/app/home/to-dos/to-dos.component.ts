59e8981accf12b57bd67ab0ce09574963bfbc428
Dar Levy <darlevy8@gmail.com>
2024-08-19 15:16:34 +0300

    Create to-dos component template

---
+++ src/app/home/to-dos/to-dos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-dos',
+  standalone: true,
+  imports: [],
+  templateUrl: './to-dos.component.html',
+  styleUrl: './to-dos.component.css'
+})
+export class ToDosComponent {
+
+}
